fc72ed3dc9bad7308be407e17e1b3092c1650ba1
JopHme <60497959+JopHme@users.noreply.github.com>
2021-04-16 23:01:12 +0700

    Succesfully retrieve current user by email

--- app/src/main/java/edu/bluejack20_2/chantuy/views/user_profile/UserProfileFragment.kt
+++ app/src/main/java/edu/bluejack20_2/chantuy/views/user_profile/UserProfileFragment.kt
@@ -1,18 +1,48 @@
 package edu.bluejack20_2.chantuy.views.user_profile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
 import edu.bluejack20_2.chantuy.R
+import edu.bluejack20_2.chantuy.models.User
+import edu.bluejack20_2.chantuy.views.CurhatAdapter
 
 
 class UserProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_user_profile, container, false)
+        val viewModel = UserProfileViewModel()
+
+        viewModel.getUser()
+//        val recyclerView: RecyclerView = rootView.findViewById(R.id.curhat_recycler_view)
+
+
+
+        val curhatAdapter = CurhatAdapter()
+        val recyclerView: RecyclerView = rootView.findViewById(R.id.recent_post_rview)
+        recyclerView.adapter = curhatAdapter
+
+
+        val nameView : TextView = rootView.findViewById(R.id.user_profile_name)
+
+        val currUserObserver = Observer<User>{newUser->
+
+            nameView.setText(viewModel.currUser?.value?.Name)
+//            curhatAdapter.submitList(curhats)
+        }
+        viewModel.currUser.observe(this,currUserObserver)
+        return rootView
+
     }
 
 
