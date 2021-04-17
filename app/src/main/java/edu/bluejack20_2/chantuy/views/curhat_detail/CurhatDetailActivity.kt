3867718a29b34efbf183857abcedbd54f1d03ed0
lionelritchie29 <lionelritchie29@github.com>
2021-04-17 14:11:16 +0700

    added curhat detail activity

---
+++ app/src/main/java/edu/bluejack20_2/chantuy/views/curhat_detail/CurhatDetailActivity.kt
@@ -0,0 +1,12 @@
+package edu.bluejack20_2.chantuy.views.curhat_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.bluejack20_2.chantuy.R
+
+class CurhatDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_curhat_detail)
+    }
+}
