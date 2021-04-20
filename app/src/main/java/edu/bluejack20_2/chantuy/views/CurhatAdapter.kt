c07cc3924ade79b02d5925fa81d75d8e7833419a
lionelritchie29 <lionelritchie29@github.com>
2021-04-20 22:13:30 +0700

    refactored curhat adapter to use data binding

--- app/src/main/java/edu/bluejack20_2/chantuy/views/CurhatAdapter.kt
+++ app/src/main/java/edu/bluejack20_2/chantuy/views/CurhatAdapter.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
@@ -18,6 +17,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import edu.bluejack20_2.chantuy.R
+import edu.bluejack20_2.chantuy.databinding.CurhatCardItemBinding
 import edu.bluejack20_2.chantuy.models.Curhat
 import edu.bluejack20_2.chantuy.models.CurhatReaction
 import edu.bluejack20_2.chantuy.repositories.CurhatCommentRepository
@@ -37,27 +37,20 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
         holder.bind(curhat)
     }
 
-    class ViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
-        private val content: TextView = view.findViewById(R.id.curhat_card_content)
-        private val username: TextView = view.findViewById(R.id.curhat_card_username)
-        private val postedDate: TextView = view.findViewById(R.id.curhat_card_date)
-        private val viewMoreBtn: Button = view.findViewById(R.id.curhat_card_view_btn)
-        private val commentCount: TextView = view.findViewById(R.id.curhat_card_comment_count)
-        private val likeBtn: ImageButton = view.findViewById(R.id.curhat_card_thumb_up_btn)
-        private val dislikeBtn: ImageButton = view.findViewById(R.id.curhat_card_thumb_down_btn)
+    class ViewHolder(var binding: CurhatCardItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(curhat: Curhat) {
-            content.text = curhat.content
-            postedDate.text = CurhatViewUtil.formatDate(curhat.createdAt)
+            binding.curhatCardContent.text = curhat.content
+            binding.curhatCardDate.text = CurhatViewUtil.formatDate(curhat.createdAt)
             CurhatRepository.incrementViewCount(curhat.id)
             setOnViewMoreListener(curhat.id)
 
             UserRepository.getUserById(curhat.user) { user ->
-                username.text = if (curhat.isAnonymous) "Anonymous" else user?.name
+                binding.curhatCardUsername.text = if (curhat.isAnonymous) "Anonymous" else user?.name
             }
 
             CurhatCommentRepository.getCommentCount(curhat.id) { count ->
-                commentCount.text = count.toString()
+                binding.curhatCardCommentCount.text = count.toString()
             }
 
 
@@ -69,15 +62,14 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
         private fun setReactionBtnColor(curhat: Curhat) {
             if (curhat == null) return
             val userId = UserRepository.getCurrentUserId()
-            Log.i("CurhatAdapter", curhat.toString())
 
             if (
                 curhat.usersGiveThumbUp?.contains(userId)!! ||
                 curhat.usersGiveCool?.contains(userId)!! ||
                 curhat.usersGiveLove?.contains(userId)!!
             ) {
-                likeBtn.setColorFilter(
-                    ContextCompat.getColor(view.context, R.color.green),
+                binding.curhatCardThumbUpBtn.setColorFilter(
+                    ContextCompat.getColor(binding.root.context, R.color.green),
                     android.graphics.PorterDuff.Mode.MULTIPLY
                 )
             }
@@ -86,8 +78,8 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                 curhat.usersGiveThumbDowns?.contains(userId)!! ||
                 curhat.usersGiveAngry?.contains(userId)!!
             ) {
-                dislikeBtn.setColorFilter(
-                    ContextCompat.getColor(view.context, R.color.dark_red),
+                binding.curhatCardThumbDownBtn.setColorFilter(
+                    ContextCompat.getColor(binding.root.context, R.color.dark_red),
                     android.graphics.PorterDuff.Mode.MULTIPLY
                 )
             }
@@ -95,8 +87,8 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
 
         @SuppressLint("RestrictedApi")
         private fun setDislikePopupMenu(id: String) {
-            dislikeBtn.setOnClickListener {
-                val popupMenu = PopupMenu(view.context, it)
+            binding.curhatCardThumbDownBtn.setOnClickListener {
+                val popupMenu = PopupMenu(binding.root.context, it)
                 popupMenu.inflate(R.menu.dislike_curhat_menu_items)
 
                 popupMenu.setOnMenuItemClickListener {
@@ -104,7 +96,7 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                         R.id.dislike_curhat_thumb_down -> {
                             CurhatRepository.addDislikeReaction(id, CurhatReaction.THUMB_DOWN) {
                                 CurhatViewUtil.changeReactionBtnColor(
-                                    dislikeBtn, R.color.dark_red, view
+                                    binding.curhatCardThumbDownBtn, R.color.dark_red, binding.root
                                 )
                             }
                             true
@@ -112,7 +104,7 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                         R.id.dislike_curhat_angry -> {
                             CurhatRepository.addDislikeReaction(id, CurhatReaction.ANGRY) {
                                 CurhatViewUtil.changeReactionBtnColor(
-                                    dislikeBtn, R.color.dark_red, view
+                                    binding.curhatCardThumbDownBtn, R.color.dark_red, binding.root
                                 )
                             }
                             true
@@ -121,7 +113,7 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                     }
                 }
 
-                val popupHelper = MenuPopupHelper(view.context, popupMenu.menu as MenuBuilder, it)
+                val popupHelper = MenuPopupHelper(binding.root.context, popupMenu.menu as MenuBuilder, it)
                 popupHelper.setForceShowIcon(true)
                 popupHelper.show()
             }
@@ -129,8 +121,8 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
 
         @SuppressLint("RestrictedApi")
         private fun setLikePopupMenu(id: String) {
-            likeBtn.setOnClickListener {
-                val popupMenu = PopupMenu(view.context, it)
+            binding.curhatCardThumbUpBtn.setOnClickListener {
+                val popupMenu = PopupMenu(binding.root.context, it)
                 popupMenu.inflate(R.menu.like_curhat_menu_items)
 
                 popupMenu.setOnMenuItemClickListener {
@@ -138,7 +130,7 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                         R.id.like_curhat_thumb_up -> {
                             CurhatRepository.addLikeReaction(id, CurhatReaction.THUMB_UP) {
                                 CurhatViewUtil.changeReactionBtnColor(
-                                    likeBtn, R.color.green, view
+                                    binding.curhatCardThumbUpBtn, R.color.green, binding.root
                                 )
                             }
                             true
@@ -146,7 +138,7 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                         R.id.like_curhat_cool -> {
                             CurhatRepository.addLikeReaction(id, CurhatReaction.COOL) {
                                 CurhatViewUtil.changeReactionBtnColor(
-                                    likeBtn, R.color.green, view
+                                    binding.curhatCardThumbUpBtn, R.color.green, binding.root
                                 )
                             }
                             true
@@ -154,7 +146,7 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                         R.id.like_curhat_love -> {
                             CurhatRepository.addLikeReaction(id, CurhatReaction.LOVE) {
                                 CurhatViewUtil.changeReactionBtnColor(
-                                    likeBtn, R.color.green, view
+                                    binding.curhatCardThumbUpBtn, R.color.green, binding.root
                                 )
                             }
                             true
@@ -164,33 +156,32 @@ class CurhatAdapter() : ListAdapter<Curhat, CurhatAdapter.ViewHolder>(CurhatDiff
                     }
                 }
 
-                val popupHelper = MenuPopupHelper(view.context, popupMenu.menu as MenuBuilder, it)
+                val popupHelper = MenuPopupHelper(binding.root.context, popupMenu.menu as MenuBuilder, it)
                 popupHelper.setForceShowIcon(true)
                 popupHelper.show()
             }
         }
 
         private fun setOnViewMoreListener(id: String) {
-            viewMoreBtn.setOnClickListener {
+            binding.curhatCardViewBtn.setOnClickListener {
                 moveToCurhatDetail(id)
             }
         }
 
         private fun moveToCurhatDetail(id: String) {
-            val intent = Intent(view.context, CurhatDetailActivity::class.java)
+            val intent = Intent(binding.root.context, CurhatDetailActivity::class.java)
             val b = Bundle()
             b.putString("id", id);
             intent.putExtras(b)
-            view.context.startActivity(intent)
+            binding.root.context.startActivity(intent)
         }
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = CurhatCardItemBinding.inflate(layoutInflater, parent, false)
 
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.curhat_card_item, parent, false)
-
-                return ViewHolder(view)
+                return ViewHolder(binding)
             }
         }
     }
